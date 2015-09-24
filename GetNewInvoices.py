878f6ecb2cb26060ebaba711d0668faebbb86c9c
Jon Hall <jon@hallusa.com>
2015-09-24 07:10:54 -0500

    Added additional script for invoices

--- GetNewInvoices.py
+++ GetNewInvoices.py
@@ -42,54 +42,11 @@ def initializeSoftLayerAPI():
     return client
 
 
-class TablePrinter(object):
-    #
-    # FORMAT TABLE
-    #
-    "Print a list of dicts as a table"
-
-    def __init__(self, fmt, sep=' ', ul=None):
-        """
-        @param fmt: list of tuple(heading, key, width)
-                        heading: str, column label
-                        key: dictionary key to value to print
-                        width: int, column width in chars
-        @param sep: string, separation between columns
-        @param ul: string, character to underline column label, or None for no underlining
-        """
-        super(TablePrinter, self).__init__()
-        self.fmt = str(sep).join('{lb}{0}:{1}{rb}'.format(key, width, lb='{', rb='}') for heading, key, width in fmt)
-        self.head = {key: heading for heading, key, width in fmt}
-        self.ul = {key: str(ul) * width for heading, key, width in fmt} if ul else None
-        self.width = {key: width for heading, key, width in fmt}
-
-    def row(self, data):
-        return self.fmt.format(**{k: str(data.get(k, ''))[:w] for k, w in self.width.items()})
-
-    def __call__(self, dataList):
-        _r = self.row
-        res = [_r(data) for data in dataList]
-        res.insert(0, _r(self.head))
-        if self.ul:
-            res.insert(1, _r(self.ul))
-        return '\n'.join(res)
-
-
 #
 # Get APIKEY from config.ini & initialize SoftLayer API
 #
 
 client = initializeSoftLayerAPI()
-
-NewInvoiceReport = [
-    ('Invoice Date', 'createDate', 12),
-    ('Invoice', 'id', 10),
-    ('Prorated Charges', 'invoiceTotalRecurringAmount',18),
-    ('One-time Amount', 'invoiceTotalOneTimeAmount', 18),
-    ('Invoice Amount', 'invoiceTotalAmount', 18),
-    ('Type', 'typeCode', 15),
-    ('Status', 'statusCode', 10)
-]
 
 
 #
@@ -99,6 +56,7 @@ print ()
 
 startdate=input("Report Start Date (MM/DD/YYYY): ")
 enddate=input("Report End Date (MM/DD/YYYY): ")
+
 
 print()
 print("Looking up invoices....")
@@ -116,31 +74,33 @@ InvoiceList = client['Account'].getInvoices(filter={
                     }
         })
 
-#print (json.dumps(InvoiceList,indent=4))
 
-data =[]
-
-
+print ()
+print ('{:<40} {:<40} {:>18} {:>16} {:>16} {:<16}'.format("Invoice Date /", "Invoice Number /", "Recurring Charges", "OneTime Charges", "Invoice Amount", "Type"))
+print ('{:<40} {:<40} {:>18} {:>16} {:>16} {:<16}'.format("Top Level Item", "Hostname        ", "                 ", "               ", "              ", "    "))
+print ('{:<40} {:<40} {:>18} {:>16} {:>16} {:<16}'.format("==============", "================", "=================", "===============", "==============", "===="))
 for invoice in InvoiceList:
     if invoice['typeCode'] == "NEW" or invoice['typeCode'] == "ONE-TIME-CHARGE":
-        InvoiceReportRow = {}
         invoiceID = invoice['id']
-        Billing_Invoice = client['Billing_Invoice'].getObject(id=invoiceID, mask="invoiceTotalAmount, invoiceTotalOneTimeAmount, invoiceTotalRecurringAmount")
-        #print (json.dumps(Billing_Invoice,indent=4))
+        Billing_Invoice = client['Billing_Invoice'].getObject(id=invoiceID, mask="invoiceTopLevelItemCount,invoiceTopLevelItems,invoiceTotalAmount, invoiceTotalOneTimeAmount, invoiceTotalRecurringAmount")
         if Billing_Invoice['invoiceTotalAmount'] > "0":
-            InvoiceReportRow['id'] = Billing_Invoice['id']
-            InvoiceReportRow['createDate'] = Billing_Invoice['createDate'][0:10]
-            InvoiceReportRow['invoiceTotalAmount'] = Billing_Invoice['invoiceTotalAmount']
-            InvoiceReportRow['invoiceTotalOneTimeAmount'] = Billing_Invoice['invoiceTotalOneTimeAmount']
-            InvoiceReportRow['invoiceTotalRecurringAmount'] = Billing_Invoice['invoiceTotalRecurringAmount']
-            InvoiceReportRow['typeCode'] = Billing_Invoice['typeCode']
-            InvoiceReportRow['statusCode'] = Billing_Invoice['statusCode']
-            data.append(InvoiceReportRow)
-
-print()
-print("                    Invoices with new Recurring & One Time Charges")
-print("                          Between %s and %s" % (startdate, enddate))
-print()
-print(TablePrinter(NewInvoiceReport, ul='=')(data))
-
-
+            # PRINT INVOICE SUMMARY
+            print ('{:40} {:<40} {:>18,.2f} {:>16,.2f} {:>16,.2f} {:<16}'.format(Billing_Invoice['createDate'][0:10], Billing_Invoice['id'], float(Billing_Invoice['invoiceTotalAmount']), float(Billing_Invoice['invoiceTotalOneTimeAmount']), float(Billing_Invoice['invoiceTotalRecurringAmount']), Billing_Invoice['typeCode']))
+            # GET associated items for recurring and onetime totals per Top Level TIem
+            for item in Billing_Invoice['invoiceTopLevelItems']:
+                associated_children = client['Billing_Invoice_Item'].getAssociatedChildren(id=item['id'])
+                recurringFee = float(item['recurringFee'])
+                oneTimeFee = float(item['oneTimeFee'])
+                for child in associated_children:
+                     #print (json.dumps(item, indent=4))
+                     recurringFee = recurringFee + float(child['recurringFee'])
+                     oneTimeFee = oneTimeFee + float(child['oneTimeFee'])
+                if 'hostName' in item:
+                    hostName = item['hostName']+"."+item['domainName']
+                else:
+                    hostName = "Unnamed Device"
+                # PRINT TOP LEVEL ITEMS DETAIL FOR  INVOICE
+                category = item["categoryCode"][0:25]
+                if recurringFee >0 or oneTimeFee > 0:
+                    print ('{:<40} {:<40} {:>18,.2f} {:>16,.2f}'.format(category[0:40],hostName[0:40], round(recurringFee,2), round(oneTimeFee,2)))
+            print()
