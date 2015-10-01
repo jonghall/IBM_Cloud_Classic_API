e6b518b195c3bec5cdf4e4312cf6d7f9866c7979
Jon Hall <jon@hallusa.com>
2015-10-01 15:57:51 -0500

    Added additional script for invoices

--- GetReccuringInvoices.py
+++ GetReccuringInvoices.py
@@ -92,9 +92,16 @@ for invoice in InvoiceList:
             print ('{:35} {:<30} {:>8} {:>16} {:>16,.2f} {:>16,.2f} {:<15}'.format(Billing_Invoice['createDate'][0:10], Billing_Invoice['id'], " ", " ", float(Billing_Invoice['invoiceTotalAmount']), float(Billing_Invoice['invoiceTotalRecurringAmount']), Billing_Invoice['typeCode']))
 
             print ()
-            print ("** HOURLY ACTUALS BILLED IN ARREARS")
+            print ("** ACTUAL HOURLY USAGE INVOICED IN ARREARS")
             print ()
             # ITERATE THROUGH DETAIL SELECTING HOURLY ITEMS
+            hourlyRecurringTotal = 0
+            totalHours = 0
+            totalItems = 0
+            maxHours = 0
+            maxRecurringFee = 0
+            minHours = 999999
+            minRecurringFee = 0
             for item in Billing_Invoice['invoiceTopLevelItems']:
                 associated_children = client['Billing_Invoice_Item'].getAssociatedChildren(id=item['id'])
 
@@ -124,13 +131,28 @@ for invoice in InvoiceList:
                             quit
                     # PRINT LINE ITEM DETAIL FOR TOP LEVEL ITEM
                     print ('{:<35} {:<30} {:>8} {:>16,.3f} {:>16,.2f}'.format(hostName[0:35],category[0:30], hours, round(hourlyRecurringFee,3), round(recurringFee,2)))
-            print()
+                    if hours > maxHours:
+                        maxHours = hours
+                        maxRecurringFee = recurringFee
 
+                    if hours < minHours:
+                        minHours = hours
+                        minRecurringFee = recurringFee
+
+                    hourlyRecurringTotal = hourlyRecurringTotal + recurringFee
+                    totalHours = totalHours + hours
+                    totalItems = totalItems + 1
+            print ()
+            print ('{:<35} {:>20} Instances {:>8} {:>16} {:>16,.2f}'.format("Hourly Totals", totalItems, totalHours, " " , round(hourlyRecurringTotal,2)))
+            print ('{:<35} {:<30} {:>8} {:>16} {:>16,.2f}'.format("Hourly Max"," ", minHours, " " , round(minRecurringFee,2)))
+            print ('{:<35} {:<30} {:>8} {:>16} {:>16,.2f}'.format("Hourly Max"," ", maxHours, " " , round(maxRecurringFee,2)))
+            print ('{:<35} {:<30} {:>8} {:>16} {:>16,.2f}'.format("Hourly Average"," ", totalHours/totalItems, " " , round(hourlyRecurringTotal/totalItems,2)))
 
             print ()
             print ("** MONTHLY & OTHER ITEMS INVOICED IN ADVANCE")
             print ()
-
+            monthlyRecurringTotal = 0
+            totalMonthlyItems = 0
             for item in Billing_Invoice['invoiceTopLevelItems']:
                 associated_children = client['Billing_Invoice_Item'].getAssociatedChildren(id=item['id'])
                 if 'hourlyRecurringFee' not in item and float(item['recurringFee'])>0:
@@ -152,9 +174,13 @@ for invoice in InvoiceList:
                             category = topLevel['name']
                             quit
                     # PRINT LINE ITEM DETAIL FOR TOP LEVEL ITEM
+                    monthlyRecurringTotal = monthlyRecurringTotal + recurringFee
+                    totalMonthlyItems = totalMonthlyItems + 1
                     print ('{:<35} {:<30} {:>8} {:>16,.3f} {:>16,.2f}'.format(hostName[0:35], category[0:30], hours, round(hourlyRecurringFee,3), round(recurringFee,2)))
             print()
-
+            print ()
+            print ('{:<35} {:<30} {:>8} {:>16} {:>16,.2f}'.format("Monthly totals"," ", " ", " " , round(monthlyRecurringTotal,2)))
+            print ()
 
 
 
