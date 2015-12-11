646f363d0f489f9476e980955b12633a75658199
Jon Hall <jon@hallusa.com>
2015-12-10 20:46:05 -0600

    Added additional script for invoices

--- VirtualServers/PowerOffVirtualServers.py
+++ VirtualServers/PowerOffVirtualServers.py
@@ -1,58 +1,46 @@
 __author__ = 'jonhall'
-#
-## Get Current Invoices
-## Place APIKEY & Username in config.ini
-## or pass via commandline  (example: PowerOffVirtualServers.py -u=userid -k=apikey)
-##
+## POWEROFF VIRtUALGUESTS
 
 import sys, getopt, socket, SoftLayer, json, string, configparser, os, argparse,csv, time
 import pandas as pd
 
-def initializeSoftLayerAPI():
-    ## READ CommandLine Arguments and load configuration file
-    parser = argparse.ArgumentParser(description="PowerOff Virtual Servers.")
-    parser.add_argument("-u", "--username", help="SoftLayer API Username")
-    parser.add_argument("-k", "--apikey", help="SoftLayer APIKEY")
-    parser.add_argument("-c", "--config", help="config.ini file to load")
-
-    args = parser.parse_args()
-
-    if args.config != None:
-        filename=args.config
-    else:
-        filename="config.ini"
-
-    if (os.path.isfile(filename) is True) and (args.username == None and args.apikey == None):
-        ## Read APIKEY from configuration file
+def initializeSoftLayerAPI(user, key, configfile):
+    if user == None and key == None:
+        if configfile != None:
+            filename=args.config
+        else:
+            filename="config.ini"
         config = configparser.ConfigParser()
         config.read(filename)
-        client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'])
+        client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'],endpoint_url=SoftLayer.API_PRIVATE_ENDPOINT)
     else:
-        ## Read APIKEY from commandline arguments
-        if args.username == None and args.apikey == None:
-            print ("You must specify a username and APIkey to use.")
-            quit()
-        if args.username == None:
-            print ("You must specify a username with your APIKEY.")
-            quit()
-        if args.apikey == None:
-            print("You must specify a APIKEY with the username.")
-            quit()
-        client = SoftLayer.Client(username=args.username, api_key=args.apikey)
+        #client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'],endpoint_url=SoftLayer.API_PRIVATE_ENDPOINT)
+        client = SoftLayer.Client(username=user, api_key=key)
     return client
 
 
-#
-# Get APIKEY from config.ini & initialize SoftLayer API
-#
+## READ CommandLine Arguments and load configuration file
+parser = argparse.ArgumentParser(description="PowerOffVirtualServers given a list of hostnames.")
+parser.add_argument("-u", "--username", help="SoftLayer API Username")
+parser.add_argument("-k", "--apikey", help="SoftLayer APIKEY")
+parser.add_argument("-c", "--config", help="config.ini file to load")
+parser.add_argument("-i", "--input", help="Outputfile")
 
-client = initializeSoftLayerAPI()
+args = parser.parse_args()
+
+client = initializeSoftLayerAPI(args.username, args.apikey, args.config)
+
+if args.input == None:
+    filename=input("Filename of servers: ")
+else:
+    filename=args.input
+
 
 ## READ CSV FILE INTO PYTHON DICTIONARY
 ## FIELDS REQUIRED: ID, HOSTNAME, WAIT
-## ID = VM ID (found via SLCLI VS LIST)
-## HOSTNAME = SL Hostname (used for display only)
-## WAIT = # of second to wait after powering off VM before moving to next VM
+## ID = VM ID (found via SLCLI VS LIST) Leave empty to have script lookup by hostname
+## HOSTNAME = SL Hostname (must be unique if you don't specify VM ID)
+## WAIT = # of second to wait after powering on VM before moving to next VM
 ## Example csv file
 ## Order,id,hostname,wait
 ## 1,13405579,  centos02 ,60
@@ -60,18 +48,34 @@ client = initializeSoftLayerAPI()
 ## 3,13405581,  centos03 ,30
 
 
-filename=input("Filename of servers: ")
+## READ ACCOUNT LIST OF VIRTUAL GUESTS
+try:
+    virtualGuests = client['Account'].getVirtualGuests(mask="id,hostname,powerState.keyName")
+except SoftLayer.SoftLayerAPIError as e:
+    print("Error: %s, %s" % (e.faultCode, e.faultString))
 
 ## OPEN CSV FILE TO READ LIST OF SERVERS
 with open(filename, 'r') as csvfile:
     serverlist = csv.DictReader(csvfile, delimiter=',', quotechar='"')
     for server in serverlist:
+        # IF ID isn't specify lookup hostname
+        print (server['id'])
+        if server['id']=="":
+            #Lookup ID
+            for virtualGuest in virtualGuests:
+                if server['hostname']==virtualGuest['hostname']:
+                    vsiid=virtualGuest['id']
+        else:
+            vsiid=server['id']
+
         ## POWER OFF SERVERS IN ORDER OF CSV FILE
-        print ("Powering off server %s (%s)" % (server['hostname'], server['id']))
+        print ("Powering off server %s (%s)" % (server['hostname'],vsiid))
+
         try:
-            poweroff = client['Virtual_Guest'].powerOffsoft(id=server['id'])
+            poweroff = client['Virtual_Guest'].powerOffsoft(id=vsiid)
         except SoftLayer.SoftLayerAPIError as e:
                 print("Error: %s, %s" % (e.faultCode, e.faultString))
+
         ## WAIT FOR PERIOD SPECIFIED
         print ("Sleeping for %s seconds" % server['wait'])
         time.sleep(float(server['wait']))
--- VirtualServers/PowerOnVirtualServers.py
+++ VirtualServers/PowerOnVirtualServers.py
@@ -1,57 +1,44 @@
 __author__ = 'jonhall'
-#
-## Get Current Invoices
-## Place APIKEY & Username in config.ini
-## or pass via commandline  (example: PowerOnVirtualServers.py -u=userid -k=apikey)
-##
+## POWERON VIRtUALGUESTS
 
-import sys, getopt, socket, SoftLayer, json, string, configparser, os, argparse, csv, time
+import sys, getopt, socket, SoftLayer, json, string, configparser, os, argparse,csv, time
+import pandas as pd
 
-
-def initializeSoftLayerAPI():
-    ## READ CommandLine Arguments and load configuration file
-    parser = argparse.ArgumentParser(description="Power-on Virtual Servers.")
-    parser.add_argument("-u", "--username", help="SoftLayer API Username")
-    parser.add_argument("-k", "--apikey", help="SoftLayer APIKEY")
-    parser.add_argument("-c", "--config", help="config.ini file to load")
-
-    args = parser.parse_args()
-
-    if args.config != None:
-        filename=args.config
-    else:
-        filename="config.ini"
-
-    if (os.path.isfile(filename) is True) and (args.username == None and args.apikey == None):
-        ## Read APIKEY from configuration file
+def initializeSoftLayerAPI(user, key, configfile):
+    if user == None and key == None:
+        if configfile != None:
+            filename=args.config
+        else:
+            filename="config.ini"
         config = configparser.ConfigParser()
         config.read(filename)
-        client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'])
+        client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'],endpoint_url=SoftLayer.API_PRIVATE_ENDPOINT)
     else:
-        ## Read APIKEY from commandline arguments
-        if args.username == None and args.apikey == None:
-            print ("You must specify a username and APIkey to use.")
-            quit()
-        if args.username == None:
-            print ("You must specify a username with your APIKEY.")
-            quit()
-        if args.apikey == None:
-            print("You must specify a APIKEY with the username.")
-            quit()
-        client = SoftLayer.Client(username=args.username, api_key=args.apikey)
+        #client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'],endpoint_url=SoftLayer.API_PRIVATE_ENDPOINT)
+        client = SoftLayer.Client(username=user, api_key=key)
     return client
 
 
-#
-# Get APIKEY from config.ini & initialize SoftLayer API
-#
+## READ CommandLine Arguments and load configuration file
+parser = argparse.ArgumentParser(description="PowerOnVirtualServers given a list of hostnames.")
+parser.add_argument("-u", "--username", help="SoftLayer API Username")
+parser.add_argument("-k", "--apikey", help="SoftLayer APIKEY")
+parser.add_argument("-c", "--config", help="config.ini file to load")
+parser.add_argument("-i", "--input", help="List of Servers")
 
-client = initializeSoftLayerAPI()
+args = parser.parse_args()
+
+client = initializeSoftLayerAPI(args.username, args.apikey, args.config)
+
+if args.input == None:
+    filename=input("Filename of servers: ")
+else:
+    filename=args.input
 
 ## READ CSV FILE INTO PYTHON DICTIONARY
 ## FIELDS REQUIRED: ID, HOSTNAME, WAIT
-## ID = VM ID (found via SLCLI VS LIST)
-## HOSTNAME = SL Hostname (used for display only)
+## ID = VM ID (found via SLCLI VS LIST) Leave empty to have script lookup by hostname
+## HOSTNAME = SL Hostname (must be unique if you don't specify VM ID)
 ## WAIT = # of second to wait after powering on VM before moving to next VM
 ## Example csv file
 ## Order,id,hostname,wait
@@ -59,20 +46,36 @@ client = initializeSoftLayerAPI()
 ## 2,13405577,  centos01 ,30
 ## 3,13405581,  centos03 ,30
 
-
-filename=input("Filename of servers: ")
+## READ ACCOUNT LIST OF VIRTUAL GUESTS
+try:
+    virtualGuests = client['Account'].getVirtualGuests(mask="id,hostname,powerState.keyName")
+except SoftLayer.SoftLayerAPIError as e:
+    print("Error: %s, %s" % (e.faultCode, e.faultString))
 
 ## OPEN CSV FILE TO READ LIST OF SERVERS
 with open(filename, 'r') as csvfile:
     serverlist = csv.DictReader(csvfile, delimiter=',', quotechar='"')
     for server in serverlist:
-        print ("Powering on server %s (%s)" % (server['hostname'], server['id']))
-        ## POWER ON VSI's IN ORDER OF CSV
+        # IF ID isn't specify lookup hostname
+        print (server['id'])
+        if server['id']=="":
+            #Lookup ID
+            for virtualGuest in virtualGuests:
+                if server['hostname']==virtualGuest['hostname']:
+                    vsiid=virtualGuest['id']
+        else:
+            vsiid=server['id']
+
+        ## POWER OFF SERVERS IN ORDER OF CSV FILE
+        print ("Powering on server %s (%s)" % (server['hostname'],vsiid))
+
         try:
-            poweroff = client['Virtual_Guest'].powerOn(id=server['id'])
+            poweron = client['Virtual_Guest'].powerOn(id=vsiid)
         except SoftLayer.SoftLayerAPIError as e:
-                print("Error: %s, %s" % (e.faultCode, e.faultString))
-        ## SLEEP FOR WAIT PERIOD SPECIFIED
+            print("Error: %s, %s" % (e.faultCode, e.faultString))
+
+        ## WAIT FOR PERIOD SPECIFIED
         print ("Sleeping for %s seconds" % server['wait'])
         time.sleep(float(server['wait']))
+
 
