61e44065c0e907c239e7f14d1948d7cd61bdc5e2
Jon Hall <jon@hallusa.com>
2015-12-10 21:05:46 -0600

    Added additional script for invoices

--- VirtualServers/ShowVirtualServerPowerState.py
+++ VirtualServers/ShowVirtualServerPowerState.py
@@ -1,54 +1,40 @@
 __author__ = 'jonhall'
-#
-## Get Current Invoices
-## Place APIKEY & Username in config.ini
-## or pass via commandline  (example: GetNewInvoices.py -u=userid -k=apikey)
-##
+## List powerstate for VirtualServers in Datacenter
 
-import sys, getopt, socket, SoftLayer, json, string, configparser, os, argparse
+import sys, getopt, socket, SoftLayer, json, string, configparser, os, argparse,csv, time
+import pandas as pd
 
-def initializeSoftLayerAPI():
-    ## READ CommandLine Arguments and load configuration file
-    parser = argparse.ArgumentParser(description="Show powerstate of Virtual Servers.")
-    parser.add_argument("-u", "--username", help="SoftLayer API Username")
-    parser.add_argument("-k", "--apikey", help="SoftLayer APIKEY")
-    parser.add_argument("-c", "--config", help="config.ini file to load")
-
-    args = parser.parse_args()
-
-    if args.config != None:
-        filename=args.config
-    else:
-        filename="config.ini"
-
-    if (os.path.isfile(filename) is True) and (args.username == None and args.apikey == None):
-        ## Read APIKEY from configuration file
+def initializeSoftLayerAPI(user, key, configfile):
+    if user == None and key == None:
+        if configfile != None:
+            filename=args.config
+        else:
+            filename="config.ini"
         config = configparser.ConfigParser()
         config.read(filename)
         client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'])
     else:
-        ## Read APIKEY from commandline arguments
-        if args.username == None and args.apikey == None:
-            print ("You must specify a username and APIkey to use.")
-            quit()
-        if args.username == None:
-            print ("You must specify a username with your APIKEY.")
-            quit()
-        if args.apikey == None:
-            print("You must specify a APIKEY with the username.")
-            quit()
-        client = SoftLayer.Client(username=args.username, api_key=args.apikey)
-
+        #client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'],endpoint_url=SoftLayer.API_PRIVATE_ENDPOINT)
+        client = SoftLayer.Client(username=user, api_key=key)
     return client
 
-#
-# Get APIKEY from config.ini & initialize SoftLayer API
-#
 
+## READ CommandLine Arguments and load configuration file
+parser = argparse.ArgumentParser(description="List powerstate for VirtualServers in Datacenter")
+parser.add_argument("-u", "--username", help="SoftLayer API Username")
+parser.add_argument("-k", "--apikey", help="SoftLayer APIKEY")
+parser.add_argument("-c", "--config", help="config.ini file to load")
+parser.add_argument("-d", "--datacenter", help="Filter by Datacenter")
 
-client = initializeSoftLayerAPI()
+args = parser.parse_args()
 
-datacenter = input ("Datacenter? ")
+client = initializeSoftLayerAPI(args.username, args.apikey, args.config)
+
+if args.datacenter == None:
+    datacenter=input("Datacenter: ")
+else:
+    datacenter=args.datacenter
+
 
 virtualServers = client['Account'].getVirtualGuests(mask='id,hostname,datacenter.name,powerState',
                                 filter={'virtualGuests': {'datacenter': {'name': {'operation': datacenter}}}})
