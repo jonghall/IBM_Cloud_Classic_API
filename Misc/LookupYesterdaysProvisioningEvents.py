65152284e804a002e4710ef2d1a3d4247350551e
Jon Hall <jon@hallusa.com>
2016-03-11 08:24:30 -0600

    Fixed differences

--- Misc/LookupYesterdaysProvisioningEvents.py
+++ Misc/LookupYesterdaysProvisioningEvents.py
@@ -1,5 +1,7 @@
-import time,  SoftLayer, configparser, os, argparse, csv, math,logging
+import sys, getopt, socket, time,  SoftLayer, json, string, configparser, os, argparse, csv, math, pytz
 from datetime import datetime, timedelta, tzinfo
+
+import sendgrid
 
 
 def convert_timedelta(duration):
@@ -31,7 +33,7 @@ def initializeSoftLayerAPI(user, key, configfile):
             filename="config.ini"
         config = configparser.ConfigParser()
         config.read(filename)
-        client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'])
+        client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'],endpoint_url=SoftLayer.API_PRIVATE_ENDPOINT)
     else:
         #client = SoftLayer.Client(username=config['api']['username'], api_key=config['api']['apikey'],endpoint_url=SoftLayer.API_PRIVATE_ENDPOINT)
         client = SoftLayer.Client(username=user, api_key=key)
@@ -44,55 +46,51 @@ def initializeSoftLayerAPI(user, key, configfile):
 
 
 ## READ CommandLine Arguments and load configuration file
-parser = argparse.ArgumentParser(description="z.")
+parser = argparse.ArgumentParser(description="Check Audit Log for VSI.")
 parser.add_argument("-u", "--username", help="SoftLayer API Username")
 parser.add_argument("-k", "--apikey", help="SoftLayer APIKEY")
 parser.add_argument("-c", "--config", help="config.ini file to load")
-parser.add_argument("-s", "--startdate", help="start date mm/dd/yy")
-parser.add_argument("-e", "--enddate", help="End date mm/dd/yyyy")
 parser.add_argument("-o", "--output", help="Outputfile")
+parser.add_argument("-d", "--date", help="Date to check.")
 args = parser.parse_args()
 
 client = initializeSoftLayerAPI(args.username, args.apikey, args.config)
 
-
-if args.startdate == None:
-    startdate=input("Report Start Date (MM/DD/YYYY): ")
+if args.date == None:
+    yesterday = datetime.now() - timedelta(days=1)
 else:
-    startdate=args.startdate
+    yesterday=datetime.strptime(args.date+" 0:0:0","%m/%d/%Y %H:%M:%S")
 
-if args.enddate == None:
-    enddate=input("Report End Date (MM/DD/YYYY): ")
-else:
-    enddate=args.enddate
+startdate = datetime.strftime(yesterday, "%m/%d/%Y") + " 0:0:0"
+enddate = datetime.strftime(yesterday,"%m/%d/%Y") + " 23:59:59"
 
 if args.output == None:
-    outputname=input("Output filename: ")
+    outputname="daily"+datetime.strftime(yesterday, "%m%d%Y")+".csv"
 else:
     outputname=args.output
 
+print ('%s Running Daily Provisioning Report for %s.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"),datetime.strftime(yesterday, "%m/%d/%Y")))
 
+distfields = ['Date','Requested', 'NotAllocated', 'NA', 'Reason', '0to30', '31-60', '61-90', '91-120', 'gt120']
 
-fieldnames = ['InvoiceID', 'BillingItemId', 'TransactionID', 'Datacenter', 'Product', 'Cores', 'Memory', 'Disk', 'OS', 'Hostname',
+fieldnames = ['InvoiceId', 'BillingItemId', 'GuestId', 'Datacenter', 'Product', 'Cores', 'Memory', 'Disk', 'OS', 'Hostname',
               'CreateDate', 'CreateTime', 'PowerOnDate', 'PowerOnTime', 'PowerOnDelta', 'ProvisionedDate',
-              'ProvisionedTime', 'ProvisionedDelta', 'CancellationDate', 'CancellationTime', 'HoursUsed', 'HourlyRecurringFee']
+              'ProvisionedTime', 'ProvisionedDelta']
 
 outfile = open(outputname, 'w')
 csvwriter = csv.DictWriter(outfile, delimiter=',', fieldnames=fieldnames)
 csvwriter.writerow(dict((fn, fn) for fn in fieldnames))
+
 ## OPEN CSV FILE TO READ LIST OF SERVERS
 
-
-logging.basicConfig(filename='lookupAllVSIProvisioningEvents.log', format='%(asctime)s %(message)s', level=logging.WARNING)
-logging.warning ('%s Checking Provisioning Events.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S")))
 
 InvoiceList = client['Account'].getInvoices(mask='createDate,typeCode, id, invoiceTotalAmount', filter={
         'invoices': {
             'createDate': {
                 'operation': 'betweenDate',
                 'options': [
-                     {'name': 'startDate', 'value': [startdate+" 0:0:0"]},
-                     {'name': 'endDate', 'value': [enddate+" 23:59:59"]}
+                     {'name': 'startDate', 'value': [startdate]},
+                     {'name': 'endDate', 'value': [enddate]}
                      ],
                 },
             'typeCode': {
@@ -105,23 +103,33 @@ InvoiceList = client['Account'].getInvoices(mask='createDate,typeCode, id, invoi
         })
 
 
-logging.info('Starting %s NEW/ONE-TIME Invoices between %s and %s.' % (len(InvoiceList),startdate,enddate))
+provisionRequests=0
+provisionRequestsgt30=0
+provisionrequestsgt30dict=[]
+powerOnNotFound=0
+timedistributiongt0=0
+timedistributiongt30=0
+timedistributiongt60=0
+timedistributiongt90=0
+timedistributiongt120=0
+
 for invoice in InvoiceList:
     invoiceID = invoice['id']
     invoicedetail=""
-    logging.warning('Getting invoice detail for invoice %s.' % (invoiceID))
+    print ('%s Looking up InvoiceId %s.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"),invoiceID))
     while invoicedetail is "":
         try:
             time.sleep(1)
             invoicedetail = client['Billing_Invoice'].getObject(id=invoiceID, mask="closedDate, invoiceTopLevelItems, invoiceTopLevelItems.product,invoiceTopLevelItems.location")
         except SoftLayer.SoftLayerAPIError as e:
-            logging.warning("Billing_Invoice::getObject: %s, %s" % (e.faultCode, e.faultString))
+            print("Billing_Invoice::getObject: %s, %s" % (e.faultCode, e.faultString))
             time.sleep(5)
 
     invoiceTopLevelItems=invoicedetail['invoiceTopLevelItems']
     invoiceDate=convert_timestamp(invoicedetail["closedDate"])
     for item in invoiceTopLevelItems:
         if item['categoryCode']=="guest_core":
+            provisionRequests=provisionRequests+1
             itemId = item['id']
             billingItemId = item['billingItemId']
             location=item['location']['name']
@@ -130,13 +138,13 @@ for invoice in InvoiceList:
             product=item['description']
             cores=""
             billing_detail=""
-            logging.warning('Getting detail for billingitemId %s.' % (billingItemId))
+            print ('%s Looking up billing Invoice Item for %s.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"),itemId))
             while billing_detail is "":
                 try:
                     time.sleep(1)
                     billing_detail = client['Billing_Invoice_Item'].getFilteredAssociatedChildren(id=itemId)
                 except SoftLayer.SoftLayerAPIError as e:
-                    logging.warning("Billing_Invoice_Item::getFilteredAssociatedChildren: %s, %s" % (e.faultCode, e.faultString))
+                    print("Billing_Invoice_Item::getFilteredAssociatedChildren: %s, %s" % (e.faultCode, e.faultString))
                     time.sleep(5)
 
             os=getDescription("os", billing_detail)
@@ -149,13 +157,13 @@ for invoice in InvoiceList:
                 cores=item['product']['totalPhysicalCoreCount']
 
             billingInvoiceItem=""
-            logging.warning('Getting item detail for itemId %s.' % (itemId))
+            print ('%s Searching for provisioning detail for billing Item for %s.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"),itemId))
             while billingInvoiceItem is "":
                 try:
                    time.sleep(1)
                    billingInvoiceItem = client['Billing_Invoice_Item'].getBillingItem(id=itemId, mask="cancellationDate, provisionTransaction")
                 except SoftLayer.SoftLayerAPIError as e:
-                   logging.warning("Billing_Invoice_Item::getBillingItem: %s, %s" % (e.faultCode, e.faultString))
+                   print("Billing_Invoice_Item::getBillingItem: %s, %s" % (e.faultCode, e.faultString))
                    time.sleep(5)
 
             if 'provisionTransaction' in billingInvoiceItem:
@@ -169,46 +177,20 @@ for invoice in InvoiceList:
                 guestId = "0"
                 provisionDateStamp = convert_timestamp(item['createDate'])
 
-            # determine cancelation date of VSI to calculate total hours; otherwise assume still running
-            if 'cancellationDate' in billingInvoiceItem:
-                if billingInvoiceItem['cancellationDate'] != "":
-                    cancellationDateStamp=convert_timestamp(billingInvoiceItem['cancellationDate'])
-            else:
-                cancellationDateStamp="Running"
-
-            # If still running use current timestamp to calculate hoursUsed otherwise use cancellation date.
-
-            if cancellationDateStamp == "Running":
-                    currentDateStamp=datetime.datetime.now()
-                    hoursUsed=math.ceil(convert_timedelta(currentDateStamp-provisionDateStamp)/60)
-            else:
-                    hoursUsed=math.ceil(convert_timedelta(cancellationDateStamp-provisionDateStamp)/60)
-
-
-            # CALCULATE HOURLY CHARGE INCLUDING ASSOCIATED CHILDREN
-            if 'hourlyRecurringFee' in item:
-                hourlyRecurringFee = round(float(item['hourlyRecurringFee']),3)
-            else:
-                hourlyRecurringFee = 0
-
-
-            for child in billing_detail:
-                if 'hourlyRecurringFee' in child:
-                    hourlyRecurringFee = round(hourlyRecurringFee + float(child['hourlyRecurringFee']),3)
-
-
-            # Search for oldest powerOn event in Event Log
             eventdate = provisionDateStamp
             powerOnDateStamp = provisionDateStamp
             found=0
-            logging.warning('Getting POWERON detail for guestId %s.' % (guestId))
+
+            print ('%s Searching event Log for POWERON detail for guestId %s.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"),guestId))
+            # GET OLDEST POWERON EVENT FROM EVENTLOG FOR GUESTID AS INITIAL RESOURCE ALLOCATION TIMESTAMP
+
             events=""
             try:
                 time.sleep(1)
                 events = client['Event_Log'].getAllObjects(mask="objectId,eventName,eventCreateDate",filter={'objectId': {'operation': guestId},
                                                          'eventName': {'operation': 'Power On'}})
             except SoftLayer.SoftLayerAPIError as e:
-                logging.warning("Event_Log::getAllObjects: %s, %s" % (e.faultCode, e.faultString))
+                print("Event_Log::getAllObjects: %s, %s" % (e.faultCode, e.faultString))
 
             for event in events:
                 if event['eventName']=="Power On":
@@ -219,6 +201,7 @@ for invoice in InvoiceList:
                         powerOnDateStamp = eventdate
                         found=1
 
+
             # FORMAT DATE & TIME STAMPS AND DELTAS FOR CSV
             createDate=datetime.strftime(createDateStamp,"%Y-%m-%d")
             createTime=datetime.strftime(createDateStamp,"%H:%M:%S")
@@ -228,27 +211,29 @@ for invoice in InvoiceList:
 
             # Calculate poweron if found
             if found==1:
+                print ('%s POWERON detail for guestId %s FOUND.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"),guestId))
                 powerOnDate=datetime.strftime(powerOnDateStamp,"%Y-%m-%d")
                 powerOnTime=datetime.strftime(powerOnDateStamp,"%H:%M:%S")
                 powerOnDelta=convert_timedelta(powerOnDateStamp-createDateStamp)
+                if powerOnDelta>30:
+                    provisionRequestsgt30=provisionRequestsgt30+1
+                    provisionrequestsgt30dict.append({
+                            'invoiceID': invoiceID,
+                            'billingItemId': billingItemId,
+                            'guestId': guestId,
+                            'powerOnDelta': powerOnDelta
+                    })
+
             else:
+                print ('%s POWERON detail for guestId %s NOT FOUND.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"),guestId))
                 powerOnDate="Not Found"
                 powerOnTime="Not Found"
-                logging.warning('POWERON detail for guestId %s NOT FOUND.' % (guestId))
                 powerOnDelta=0
+                powerOnNotFound=powerOnNotFound+1
 
-            if cancellationDateStamp=="Running":
-                cancellationDate="Running"
-                cancellationTime="Running"
-            else:
-                cancellationDate=datetime.strftime(cancellationDateStamp,"%Y-%m-%d")
-                cancellationTime=datetime.strftime(cancellationDateStamp,"%H:%M:%S")
-
-            # Create CSV Record
-            logging.warning('Writting record for InvoiceId %s, BillingItemId %s, GuestId %s.' % (invoiceID, billingItemId, guestId))
-            row = {'InvoiceID': invoiceID,
+            row = {'InvoiceId': invoiceID,
                    'BillingItemId': billingItemId,
-                   'TransactionID': guestId,
+                   'GuestId': guestId,
                    'Datacenter': location,
                    'Product': product,
                    'Cores': cores,
@@ -263,15 +248,53 @@ for invoice in InvoiceList:
                    'PowerOnDelta': powerOnDelta,
                    'ProvisionedDate': provisionDate,
                    'ProvisionedTime': provisionTime,
-                   'ProvisionedDelta': provisionDelta,
-                   'CancellationDate': cancellationDate,
-                   'CancellationTime': cancellationTime,
-                   'HoursUsed': hoursUsed,
-                   'HourlyRecurringFee': hourlyRecurringFee,
+                   'ProvisionedDelta': provisionDelta
                    }
-
             csvwriter.writerow(row)
+            # Create Provisioning Time Distirbution
+            if provisionDelta>=121:
+                timedistributiongt120=timedistributiongt120+1
+            elif provisionDelta>=91:
+                timedistributiongt90=timedistributiongt90+1
+            elif provisionDelta>=61:
+                timedistributiongt60=timedistributiongt60+1
+            elif provisionDelta>=31:
+                timedistributiongt30=timedistributiongt30+1
+            elif provisionDelta>0:
+                timedistributiongt0=timedistributiongt0+1
+
+            print ('%s Added Invoice %s GuestId %s to CSV file.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"), invoiceID, guestId))
+
 
 ##close CSV File
-logging.info('Finished %s NEW/ONE-TIME Invoices between %s and %s.' % (len(InvoiceList),startdate,enddate))
 outfile.close()
+
+# FORMAT & SEND EMAIL VIA SENDGRID ACCOUNT
+sg = sendgrid.SendGridClient('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')
+message = sendgrid.Mail()
+message.add_to(['Jon Hall <jonhall@us.ibm.com>','Michelle Chank <mchank@us.ibm.com>', ' Julie V Opheim <vennette@us.ibm.com>'])
+#message.add_to('Jon Hall <jonhall@us.ibm.com>')
+message.set_subject('AFI Daily Provisioning Report')
+body=('<style>table, th, td {border: 1px solid black;}</style><p>AFI Provisioning detail for %s.<br/>Total Provisioning Requests = %s.<br/>' \
+      'PowerOn events exceeding 30 minutes = %s.<br/>PowerOn events Not Found = %s.</p>' \
+       % (datetime.strftime(yesterday, "%m/%d/%Y"),provisionRequests, provisionRequestsgt30,powerOnNotFound))
+
+body=body+('<p>Time Distribution Export<br><table width="200"><tr>' \
+           '<th>Date</th><th>Requested</th><th>NotAllocated</th><th>0to30</th><th>31-60</th><th>61-90</th><th>91-120</th><th>gt120</th></tr>' \
+           '<td style="text-align: center;">%s</td><td style="text-align: center;">%s</dh><td style="text-align: center;">%s</td>' \
+           '<td style="text-align: center;">%s</td><td style="text-align: center;">%s</td><td style="text-align: center;">%s</td>' \
+           '<td style="text-align: center;">%s</td><td style="text-align: center;">%s</td></tr></table></p>'
+            % (datetime.strftime(yesterday, "%d-%b"),provisionRequests,provisionRequestsgt30,timedistributiongt0,timedistributiongt30,timedistributiongt60,timedistributiongt90,timedistributiongt120))
+
+if provisionRequestsgt30>0:
+            body=body+('<table width="600"><tr><th>%s</th><th>%s</th><th>%s</th><th>%s</th></tr>' % ("InvoiceID","BillingItemId","GuestId", "PowerOnDelta"))
+            for list in provisionrequestsgt30dict:
+                body=body+('<tr><td style="text-align: center;">%s</td><td style="text-align: center;">%s</td><td style="text-align: center;">%s</td><td style="text-align: center;">%s</td></tr>' % (list['invoiceID'],list['billingItemId'],list['guestId'],list['powerOnDelta']))
+            body=body+"</table>"
+
+message.set_html(body)
+message.set_from('Jon Hall <jonhall@us.ibm.com>')
+message.add_attachment(outputname, './'+outputname)
+status, msg = sg.send(message)
+
+print ('%s Sending Daily Provisioning Report for %s.' % (datetime.strftime(datetime.now(),"%m/%d/%Y %H:%M:%S"),datetime.strftime(yesterday, "%m/%d/%Y")))
